c0057e0ba5224606e59eecebec51c9dee1f4ea88
IreneCreadora <nargiza.dancer@gmail.com>
2022-12-05 17:23:51 +0200

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { Layout } from './Layout';
 
 import { ContactForm } from './ContactForm/contactForm';
